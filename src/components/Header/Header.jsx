e72f29439605d99b05c1ae0e14427e6025e0b648
Violet Stewart <vkazoo2@gmail.com>
2022-02-24 09:54:45 -0800

    some user logic in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useUser } from '../../Context/UserContext';
 
 export default function Header() {
-  return <div>Header</div>;
+  const { user } = useUser();
+  if (user) return <h1>Hi {user}</h1>;
+  return (
+    <div>
+      <h1>Acme Employee Directory</h1>
+      <button>Sign In</button>
+    </div>
+  );
 }
