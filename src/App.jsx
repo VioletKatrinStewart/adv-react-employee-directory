f184eb8e4dc10b9447f4d0e0192fe7b503f01405
Violet Stewart <vkazoo2@gmail.com>
2022-02-24 09:42:50 -0800

    set up routes in app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Auth from './views/Auth/Auth';
+import Profile from './views/Profile/Profile';
+import Header from './components/Header/Header';
+import Home from './views/Home/Home';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Auth />
+          </Route>
+          <Route path="/register">
+            <Auth />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  return (
+    <div>
+      <h1>Auth Page, SIGN IN AND SIGN OUT</h1>
+    </div>
+  );
 }
